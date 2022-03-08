8ba52d22741192fbaff9787a11859f6209e68786
ClaudiaWei <claudia84012345@gmail.com>
2022-03-09 01:13:32 +0800

    feature: add create_postgres_table dag

---
+++ dags/create_postgres_table.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="postgres_operator_dag",
+    start_date=datetime.datetime(2020,3,8),
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+    create_pet_table = PostgresOperator(
+        task_id="create_record_table",
+        sql="""
+            CREATE TABLE IF NOT EXISTS record (
+            id SERIAL PRIMARY KEY,
+            user_id INT NOT NULL,
+            name VARCHAR NOT NULL,
+            score INT NOT NULL,
+            create_at DATE NOT NULL);
+          """,
+    )
